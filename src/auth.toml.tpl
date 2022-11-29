c9f3bbbefa9f750cc0b6e25f46fbc4d60234a89c
dpetravick <dpetravick@yahoo.com>
2022-11-29 10:26:11 -0600

    rem of legacy files

--- src/auth.toml.tpl
+++
@@ -1,7 +0,0 @@
-[[auth]]
-username = "{{ HOP_USERNAME }}"
-password = "{{ HOP_PASSWORD }}"
-protocol = "SASL_SSL"
-mechanism = "SCRAM-SHA-512"
-hostname = "kafka.scimma.org"
-ssl_ca_location = "/home/worker/.local/lib/python3.8/site-packages/certifi/cacert.pem"
--- src/copy_auth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir -p /home/worker/.config/hop
-sed "s/{{ HOP_USERNAME }}/${HOP_USERNAME}/g" /home/worker/src/auth.toml.tpl | sed "s/{{ HOP_PASSWORD }}/${HOP_PASSWORD}/g" | sed "s#{{ HOME }}#${HOME}#g" > /home/worker/.config/hop/auth.toml
-chmod 0600 /home/worker/.config/hop/auth.toml
-
