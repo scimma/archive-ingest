458b8a048d28c2ec8c08fd0ee44e01e09fa96ae9
T. Andrew Manning <manninga@illinois.edu>
2023-04-05 16:14:12 -0500

    Remove React.StrictMode to avoid double click triggers

--- browser/src/src/index.js
+++ browser/src/src/index.js
@@ -5,7 +5,7 @@ import {default as App} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	// <React.StrictMode>
 		<App />
-	</React.StrictMode>
+	// </React.StrictMode>
 );
