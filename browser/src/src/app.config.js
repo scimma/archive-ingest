f74c1f8ec6cc511e30816aa08be32d7edef3ef5d
T. Andrew Manning <manninga@illinois.edu>
2023-04-07 17:14:14 -0500

    Add basepath option for browser to connect to api server

--- browser/src/src/app.config.js
+++ browser/src/src/app.config.js
@@ -2,11 +2,16 @@ let config = {};
 let hostname = process.env.REACT_APP_REMOTE_HOSTNAME || "127.0.0.1";
 let port = process.env.REACT_APP_REMOTE_PORT || "8000";
 let protocol = process.env.REACT_APP_REMOTE_PROTOCOL || "http";
+let apiBasePath = process.env.REACT_APP_REMOTE_BASEPATH || "api";
 
 config["hostname"] = hostname;
 config["port"] = port;
 config["protocol"] = protocol;
 config["baseUrl"] = '/';
-config["baseApiUrl"] = '/api';
+if (apiBasePath === "") {
+    config["baseApiUrl"] = `/api`;
+} else {
+    config["baseApiUrl"] = `/${apiBasePath}/api`;
+}
 
 export default config;
